1d6498aaf53c36b7bea896d868ecb974d0feb65b
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-18 15:11:54 -0600

    141 - Link componente

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
 	return (
 		<>
 			<div className="navbar navbar-dark bg-dark">
 				<div className="container">
-					<a href="/" className="btn btn-outline-primary">Inicio</a>
-					<a href="/nosotros" className="btn btn-outline-primary">Nosotros</a>
-					<a href="/contacto" className="btn btn-outline-primary">Contacto</a>
-					<a href="/blog" className="btn btn-outline-primary">Blog</a>
+					<Link className="btn btn-outline-primary" to="/">Inicio</Link>
+					<Link className="btn btn-outline-primary" to="/nosotros">Nosotros</Link>
+					<Link className="btn btn-outline-primary" to="/contacto">Contacto</Link>
+					<Link className="btn btn-outline-primary" to="/blog">Blog</Link>
 				</div>
 			</div>
 		</>
