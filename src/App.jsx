bf52c6ef91d575201b3f85f981470305b169f086
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-18 17:05:50 -0600

    143 - Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from 'react-router-dom'
 
+import LayoutPublic from "./layouts/LayoutPublic";
 import Inicio from './pages/Inicio';
 import Nosotros from './pages/Nosotros';
 import Contacto from './pages/Contacto';
 import Blog from './pages/Blog';
 
 const App = () => { 
-  return <>
-    <Navbar></Navbar>
-    <h1>APP</h1>
-
+  return (<>
+    <Navbar />
     <Routes>
-      <Route element={<Inicio/>} path="/"></Route>
-      <Route element={<Nosotros/>} path="/nosotros"></Route>
-      <Route element={<Contacto/>} path="/contacto"></Route>
-      <Route element={<Blog/>} path="/blog"></Route>
+      <Route path="/" element={<LayoutPublic />}>
+        <Route element={<Inicio />} index></Route>
+        <Route element={<Nosotros />} path="/nosotros"></Route>
+        <Route element={<Contacto />} path="/contacto"></Route>
+        <Route element={<Blog />} path="/blog"></Route>
+      </Route>
     </Routes>
-  </>
+  </>)
 }
 
 export default App;
