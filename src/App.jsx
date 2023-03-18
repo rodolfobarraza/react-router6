5bcd1ed7b5ee91c421b235af107d14705245454f
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-18 16:20:40 -0600

    142 - Configurar Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from 'react-router-dom'
+
+import Inicio from './pages/Inicio';
+import Nosotros from './pages/Nosotros';
+import Contacto from './pages/Contacto';
+import Blog from './pages/Blog';
 
 const App = () => { 
   return <>
     <Navbar></Navbar>
     <h1>APP</h1>
+
+    <Routes>
+      <Route element={<Inicio/>} path="/"></Route>
+      <Route element={<Nosotros/>} path="/nosotros"></Route>
+      <Route element={<Contacto/>} path="/contacto"></Route>
+      <Route element={<Blog/>} path="/blog"></Route>
+    </Routes>
   </>
 }
 
