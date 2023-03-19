8adefa1d104f2fa39f8feb9f27cdfd32f1d02dfd
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-18 22:31:28 -0600

    147 - useParams

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,5 +1,27 @@
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
 const BlogDetails = () => { 
-  return "BlogDetails";
- }
+  let params = useParams();
+    const { data, error, loading } = useFetch('https://jsonplaceholder.typicode.com/posts/' + params.id);
+
+    if (loading) {
+      return <h1>Cargando...</h1>
+    }
+  
+    if (error) {
+      return <h1>{ error }</h1>
+    }
+  
+    return (
+      <>
+        <h1>
+          { data.id } - { data.title }
+        </h1>
+        <p>{ data.body }</p>
+        <Link to="/blog" className="btn btn-dark">Regresar</Link>
+      </>
+    )
+  }
 
 export default BlogDetails;
